50cc872d198d6175898c704400c0ebe8522f127e
Volodymyr Yahello <vyahello@gmail.com>
2022-02-03 21:53:58 +0200

    Add project skeleton

---
+++ app/app/views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render
+from app.forms import UserForm,UserProfileInfoForm
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'app/index.html')
+
+
+@login_required
+def special(request):
+    return HttpResponse('You are logged in!')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+def register(request):
+
+    registered = False
+
+    if request.method == 'POST':
+
+        # Get info from "both" forms
+        # It appears as one form to the user on the .html page
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        # Check to see both forms are valid
+        if user_form.is_valid() and profile_form.is_valid():
+
+            # Save User Form to Database
+            user = user_form.save()
+
+            # Hash the password
+            user.set_password(user.password)
+
+            # Update with Hashed password
+            user.save()
+
+            # Now we deal with the extra info!
+
+            # Can't commit yet because we still need to manipulate
+            profile = profile_form.save(commit=False)
+
+            # Set One to One relationship between
+            # UserForm and UserProfileInfoForm
+            profile.user = user
+
+            # Check if they provided a profile picture
+            if 'profile_pic' in request.FILES:
+                print('found it')
+                # If yes, then grab it from the POST form reply
+                profile.profile_pic = request.FILES['profile_pic']
+
+            # Now save model
+            profile.save()
+
+            # Registration Successful!
+            registered = True
+
+        else:
+            # One of the forms was invalid if this else gets called.
+            print(user_form.errors,profile_form.errors)
+
+    else:
+        # Was not an HTTP post so we just render the forms as blank.
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    # This is the render and context dictionary to feed
+    # back to the registration.html file page.
+    return render(request,'app/registration.html',
+                          {'user_form':user_form,
+                           'profile_form':profile_form,
+                           'registered':registered})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+            else:
+                return HttpResponse('Account not active')
+        else:
+            print('Someone tried to login and failed')
+            print(f'{username} - {password}')
+            return HttpResponse('Invalid login details')
+    else:
+        return render(request, 'app/login.html', {})
